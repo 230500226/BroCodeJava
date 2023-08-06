d5db2e83f438d256152c15bfb760bd57da66579d
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2023-08-06 14:22:36 +0200

    init

---
+++ HelloWorldProject/HelloWorldProject/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        notMain(args);
+    }
+    
+    public static void notMain(String[] args){
+        System.out.println("This language is so objectively fuked");
+        System.out.println("But hey, so is this country right?");
+    }
+}
